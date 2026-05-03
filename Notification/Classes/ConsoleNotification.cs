4fc36d74167e64b05d1752a1d7c0f4d24efc9c5f
Martin Bosančić <martinbosancic1@gmail.com>
2026-05-03 13:35:19 +0200

    Add files via upload

---
+++ Notification/Classes/ConsoleNotification.cs
@@ -0,0 +1,37 @@
+﻿using Notification.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Classes
+{
+    public enum Category { ERROR, ALERT, INFO }
+    public class ConsoleNotification : Prototype
+    {
+        public String Author { get; private set; }
+        public String Title { get; private set; }
+        public String Text { get; private set; }
+        public DateTime Timestamp { get; private set; }
+        public Category Level { get; private set; }
+        public ConsoleColor Color { get; private set; }
+
+        public ConsoleNotification(String author, String title,
+        String text, DateTime time, Category level, ConsoleColor color)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Text = text;
+            this.Timestamp = time;
+            this.Level = level;
+            this.Color = color;
+        }
+
+        public Prototype Clone()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+
+}
